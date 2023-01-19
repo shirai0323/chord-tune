3122f328e81be10fd72b66f7e06cab19686da509
白井 <101955192+shirai0323@users.noreply.github.com>
2023-01-19 18:54:18 +0900

    Revert "[Add]ログイン・ログアウト機能の実装"

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   root 'top#index'
 
-  get 'login', to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
+  get 'new', to: 'users#new'
 
   resources :users, only: %i[new create]
 end
