f0ee4a51a417757d228502e54fd9be91992f2ba6
shirai0323 <a.shirai0323@gmail.com>
2023-01-21 12:07:20 +0900

    [Add]Postコントトーラーの作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+  end
+
+  def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :song_title, :capotast)
+  end
+end
