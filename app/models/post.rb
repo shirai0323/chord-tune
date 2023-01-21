ece9a076bfdbffb14a87e68ffc1dbf1235dbb0f5
shirai0323 <a.shirai0323@gmail.com>
2023-01-21 16:12:02 +0900

    [Add]アソシエーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 
+  belongs_to :user
+
   validates :body, presence: true, length: { maximum: 65_535 }
   validates :song_title, presence: true, length: { maximum: 255 }
 end
