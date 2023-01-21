a427d51b60e3e806b2e58594367334c26789f5c5
shirai0323 <a.shirai0323@gmail.com>
2023-01-21 11:56:07 +0900

    [Add]Postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_025115) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_025231) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "body"
+    t.string "song_title"
+    t.integer "capotast"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
