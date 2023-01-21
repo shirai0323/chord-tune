e26e616376666a779d236eaa60c267fa7096e687
shirai0323 <a.shirai0323@gmail.com>
2023-01-21 16:40:24 +0900

    [Add]Airtistモデルの作成

---
+++ app/models/airtist.rb
@@ -0,0 +1,2 @@
+class Airtist < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_025231) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_073418) do
+  create_table "airtists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "body"
     t.string "song_title"
