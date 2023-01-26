45c23d9b51639ea2196b97daa826a5026424d790
shirai0323 <a.shirai0323@gmail.com>
2023-01-26 15:43:11 +0900

    [Fix]コード譜新規作成画面の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.save
-    redirect_to posts_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now['danger'] = 'error'
+      render :new
+    end
   end
 
   def edit
