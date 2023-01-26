5c3385399b587c091b1eecd025f682c7ec5a6c74
shirai0323 <a.shirai0323@gmail.com>
2023-01-27 00:34:21 +0900

    [Fix]saveメソッド変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save!
+    if @post.save
       redirect_to posts_path
     else
       falsh.now['danger'] = 'error'
