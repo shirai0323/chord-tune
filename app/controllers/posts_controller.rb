d1b631b72dc9822a48d250d054e9dce282eb6256
shirai0323 <a.shirai0323@gmail.com>
2023-02-03 16:51:15 +0900

    [Add]編集・削除メソッドの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,23 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      flash.now['danger'] = 'error'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destory!
+    redirect_to posts_path
   end
 
   private
