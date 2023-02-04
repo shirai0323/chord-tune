e4b83f1f931ae8300b0af51f75831ea36a470bf8
shirai0323 <a.shirai0323@gmail.com>
2023-02-04 22:24:32 +0900

    [Add]postにユーザーID外部キーを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_154618) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_131836) do
   create_table "airtists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_154618) do
     t.integer "capotast"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "scores", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -96,5 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_154618) do
   add_foreign_key "music_genres", "posts"
   add_foreign_key "post_airtists", "airtists"
   add_foreign_key "post_airtists", "posts"
+  add_foreign_key "posts", "users"
   add_foreign_key "scores", "posts"
 end
