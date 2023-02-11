38e57a9093ed4496d1e858c522b1d1306bc8ae82
shirai0323 <a.shirai0323@gmail.com>
2023-02-11 16:05:49 +0900

    [Add]avatarカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_131836) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_070431) do
   create_table "airtists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_131836) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
