7ed11313c49387caa9c01822575d88906bc802ce
shirai0323 <a.shirai0323@gmail.com>
2023-02-17 14:45:39 +0900

    [Add]fontawesome導入

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.3.0/js/all.js"
