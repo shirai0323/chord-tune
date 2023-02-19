dd01bd65399b95a1fd4e2c5772ac7fb641995d8d
shirai0323 <a.shirai0323@gmail.com>
2023-02-19 10:59:59 +0900

    [Add]CSRF対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
   before_action :set_search
+  protect_from_forgery with: :exception
 
   def set_search
     @q = Post.ransack(params[:q])
