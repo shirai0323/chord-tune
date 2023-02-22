2864939ae055b8fdf5e443704d0f7af44a2a2862
shirai0323 <a.shirai0323@gmail.com>
2023-02-22 14:49:07 +0900

    [Add]Songモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_022335) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_054515) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -50,6 +50,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_022335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_scores_on_post_id"
+  end
+
+  create_table "songs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "airtist", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
