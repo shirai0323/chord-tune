41555f648db37db4fd7ddfee3e45c5f7a0a76cda
shirai0323 <a.shirai0323@gmail.com>
2023-03-02 17:38:00 +0900

    [Fix]Songモデル周り修正

--- config/initializers/rest-client.rb
+++
@@ -1,9 +0,0 @@
-RestClient.log =
-  Object.new.tap do |proxy|
-    def proxy.<<(message)
-      Rails.logger.info message
-    end
-  end
-
-  logfile = Rails.root.join('log', 'restclient.log')
-RestClient.log = Logger.new(logfile)
