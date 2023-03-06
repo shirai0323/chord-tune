7d4e2a33a75562bd68800e816bcac96dcd7249bf
shirai0323 <a.shirai0323@gmail.com>
2023-03-07 01:25:31 +0900

    [Add]本番環境のSSL接続設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get "/pages/*id" => 'pages#about', as: :pages, format: false
 
   resources :users, only: %i[new create]
   resources :posts do
