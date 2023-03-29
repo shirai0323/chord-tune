d7e998edcbb12efb7a157518bc17c92508c480d7
shirai0323 <a.shirai0323@gmail.com>
2023-03-29 17:48:24 +0900

    [Fix]エラー文修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
+    @user&.deliver_reset_password_instructions!
     redirect_to login_path
   end
 
